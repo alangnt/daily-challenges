90ff008951173b1311705a38a1c236266f6c3762
Sonny Li <sonnynomnom@gmail.com>
2026-03-13 05:40:38 -0400

    Update homebrew_computer_club.py

--- march-2026/3-05-homebrew-computer-club/homebrew_computer_club.py
+++ march-2026/3-05-homebrew-computer-club/homebrew_computer_club.py
@@ -2,7 +2,6 @@
 # Codédex
 
 def dompier_music(switches):
-  # Write code below 💖
   result = []
 
   for i in range(len(switches)):
@@ -13,22 +12,22 @@ def dompier_music(switches):
         decimal = decimal + 2 ** (9-int(j))
 
     if decimal == 261:
-      result.append("C4")
+      result.append('C4')
     elif decimal == 294:
-      result.append("D4")
+      result.append('D4')
     elif decimal == 329:
-      result.append("E4")
+      result.append('E4')
     elif decimal == 349:
-      result.append("F4")
+      result.append('F4')
     elif decimal == 392:
-      result.append("G4")
+      result.append('G4')
     elif decimal == 440:
-      result.append("A4")
+      result.append('A4')
     elif decimal == 494:
-      result.append("B4")
+      result.append('B4')
     elif decimal == 523:
-      result.append("C5")
+      result.append('C5')
     else:
-      result.append("REST")
+      result.append('REST')
 
   return result
